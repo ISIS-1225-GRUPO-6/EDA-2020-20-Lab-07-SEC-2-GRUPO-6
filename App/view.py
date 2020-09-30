c88f216e43f455c7efa378c01fe1d9e28036be2d
luisa fuentes <l.fuentesl@uniandes.edu.co>
2020-09-29 22:19:55 -0500

    Carga todo pero corregido por el profe

--- App/view.py
+++ App/view.py
@@ -22,6 +22,7 @@
 
 import sys
 import config
+from DISClib.ADT import list as lt
 from App import controller
 assert config
 
@@ -37,7 +38,7 @@ operación seleccionada.
 # ___________________________________________________
 
 
-crimefile = 'crime-utf8.csv'
+accidentsfile = 'us_accidents_small.csv'
 
 # ___________________________________________________
 #  Menu principal
@@ -69,10 +70,17 @@ while True:
         cont = controller.init()
 
     elif int(inputs[0]) == 2:
-        print("\nCargando información de crimenes ....")
+        print("\nCargando información de accidentes ....")
+        controller.loadData(cont,accidentsfile)
+        print('Accidentes cargados: ' + str(controller.accisSize(cont)))
+        print('Altura del arbol: ' + str(controller.indexHeight(cont)))
+        print('Elementos en el arbol: ' + str(controller.indexSize(cont)))
+        print('Menor Llave: ' + str(controller.minKey(cont)))
+        print('Mayor Llave: ' + str(controller.maxKey(cont)))
 
     elif int(inputs[0]) == 3:
         print("\nBuscando crimenes en un rango de fechas: ")
+
 
     elif int(inputs[0]) == 4:
         print("\nRequerimiento No 1 del reto 3: ")
